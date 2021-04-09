0a6608eeaef88b183fb62eeedc42c898318c5d6e
Liam Cusack <LCusack8108@gmail.com>
2021-04-08 19:06:47 -0600

    Lost in graphiql setup woods
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
     field :create_user, mutation: Mutations::CreateUser
+    field :create_tour, mutation: Mutations::CreateTour
   end
 end
