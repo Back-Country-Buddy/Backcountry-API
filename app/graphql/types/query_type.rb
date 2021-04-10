69be999cde8cb2ccbeca32a2c83c923369aff166
awosborn <alexander.w.osborne@gmail.com>
2021-04-10 14:44:55 -0600

    Add fetchUser query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,5 +4,6 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :fetch_users, resolver: Queries::FetchUsers
+    field :fetch_user, resolver: Queries::FetchUser
   end
 end
