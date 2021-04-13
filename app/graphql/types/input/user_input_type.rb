29d190a3e6c7f3ccba590270cc77ccbafb3366fb
awosborn <alexander.w.osborne@gmail.com>
2021-04-13 16:32:44 -0600

    Add UserDelete

--- app/graphql/types/input/user_input_type.rb
+++ app/graphql/types/input/user_input_type.rb
@@ -2,11 +2,11 @@ module Types
   module Input
     class UserInputType < Types::BaseInputObject
       argument :id, ID, required: false
-      argument :username, String, required: true
-      argument :email_address, String, required: true
-      argument :emergency_contact_name, String, required: true
-      argument :emergency_number, String, required: true
-      argument :friends, String, required: true
+      argument :username, String, required: false
+      argument :email_address, String, required: false
+      argument :emergency_contact_name, String, required: false
+      argument :emergency_number, String, required: false
+      argument :friends, String, required: false
     end
   end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :add_user, mutation: Mutations::AddUser
     field :update_user, mutation: Mutations::UpdateUser
+    field :delete_user, mutation: Mutations::DeleteUser
   end
 end
